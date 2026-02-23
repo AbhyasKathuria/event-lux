957a823361391cbab2508fd5c2aa82d075f45a66
Abhyas Kathuria <kathuriaabhyas@gmail.com>
2026-02-23 13:48:53 +0530

    Fix: Fully migrated to Prisma 7 config

--- apps/web/prisma.config.ts
+++ apps/web/prisma.config.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config'
-import { defineConfig } from 'prisma/config'
+import { defineConfig } from '@prisma/config'
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
-    url: process.env.DIRECT_URL!,
+    // We changed this to DATABASE_URL to match your environment variables
+    url: process.env.DATABASE_URL!,
   },
 })
