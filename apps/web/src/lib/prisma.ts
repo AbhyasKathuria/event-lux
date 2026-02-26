8da00f0cc826099295eeb60a31a814cf0bd56031
Abhyas Kathuria <kathuriaabhyas@gmail.com>
2026-02-26 23:13:02 +0530

    Remove Neon adapter - use standard PrismaClient

--- apps/web/src/lib/prisma.ts
+++ apps/web/src/lib/prisma.ts
@@ -1,24 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { neon } from "@neondatabase/serverless";
 
 declare global {
-  var prismaClient: PrismaClient | undefined;
+  var __prisma: PrismaClient | undefined;
 }
 
-function getPrismaClient(): PrismaClient {
-  if (!global.prismaClient) {
-    const url = process.env.DATABASE_URL;
-    if (!url) throw new Error("DATABASE_URL is not defined");
-    const sql = neon(url);
-    const adapter = new PrismaNeon(sql as any);
-    global.prismaClient = new PrismaClient({ adapter } as any);
-  }
-  return global.prismaClient;
+if (!global.__prisma) {
+  global.__prisma = new PrismaClient();
 }
 
-export const prisma = new Proxy({} as PrismaClient, {
-  get(_target, prop) {
-    return (getPrismaClient() as any)[prop];
-  },
-});
+export const prisma = global.__prisma;
